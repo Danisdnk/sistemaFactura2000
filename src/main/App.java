b2cc689182c61dffd247566dd48f2c934164f502
Leandro Herenu <lherenu@coalfire.com>
2021-06-05 11:04:23 -0300

    .

---
+++ src/main/App.java
@@ -0,0 +1,12 @@
+package main;
+
+import views.CiudadConMasPasajeros;
+
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+        JFrame ciudadConMasPasajeros = new CiudadConMasPasajeros("Vuelos y Aeropuertos");
+        ciudadConMasPasajeros.setVisible(true);
+    }
+}
