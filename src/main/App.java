c056016f755689960b080a3d5f2f21af94894e55
Leandro Herenu <lherenu@coalfire.com>
2021-06-05 11:12:24 -0300

    Alive

--- src/main/App.java
+++ src/main/App.java
@@ -1,12 +1,12 @@
 package main;
 
-import views.CiudadConMasPasajeros;
-
 import javax.swing.*;
 
 public class App {
     public static void main(String[] args) {
-        JFrame ciudadConMasPasajeros = new CiudadConMasPasajeros("Vuelos y Aeropuertos");
-        ciudadConMasPasajeros.setVisible(true);
+        //JFrame ciudadConMasPasajeros = new CiudadConMasPasajeros("Vuelos y Aeropuertos");
+        //ciudadConMasPasajeros.setVisible(true);
+
+        System.out.println("I'm alive");
     }
 }
