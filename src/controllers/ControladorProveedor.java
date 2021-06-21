0e52e15854496b047eafc250a1258b4b578847bd
Leandro Herenu <lherenu@coalfire.com>
2021-06-21 20:32:17 -0300

    agregando proveedores

--- src/controllers/ControladorProveedor.java
+++ src/controllers/ControladorProveedor.java
@@ -1,27 +1,48 @@
 package controllers;
+import models.dtos.DDLItemDTO;
+import models.mediopago.TipoPago;
 import models.proveedor.Proveedor;
 
 import java.util.ArrayList;
 import java.util.List;
 public class ControladorProveedor {
-
     private static ControladorProveedor instancia;
+
     private int indexer = 1;
-    private List<Proveedor> proveedor;
+    private List<Proveedor> proveedores;
 
     public ControladorProveedor() {
-        this.proveedor = new ArrayList<Proveedor>();
-        this.agregarP(new Proveedor());
+        this.proveedores = new ArrayList<Proveedor>();
+        this.agregarProveedor(new Proveedor("Coto"));
+        this.agregarProveedor(new Proveedor("YPF"));
+        this.agregarProveedor(new Proveedor("Firulais"));
+        this.agregarProveedor(new Proveedor("Philips"));
     }
 
-    public void agregarP (Proveedor p){
-        /*p.setOpID(this.indexer);*/
-        this.proveedor.add(p);
+    public void agregarProveedor(Proveedor p){
+        p.setID(this.indexer);
+        this.proveedores.add(p);
         this.indexer++;
     }
 
-    public List<Proveedor> getOPs() {
-        return this.proveedor;
+    public List<Proveedor> getProveedores() {
+        return this.proveedores;
+    }
+
+    public Proveedor getProveedorByNombre(String nombre) {
+        return this.proveedores
+                .stream()
+                .filter(p -> p.getNombre() == nombre)
+                .findFirst()
+                .get();
+    }
+
+    // TODO agregar a diagrama clases
+    public List<DDLItemDTO> getOpcionesDDLProveedores() {
+        return this.proveedores
+                .stream()
+                .map(Proveedor::toDDL)
+                .toList();
     }
 
     public static ControladorProveedor getInstancia() {
