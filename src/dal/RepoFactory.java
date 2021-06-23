ace30c20e3f06311d4bdd1fb25558245f2759a8d
Leandro Herenu <lherenu@coalfire.com>
2021-06-23 16:48:59 -0300

    factory de repositorios

---
+++ src/dal/RepoFactory.java
@@ -0,0 +1,24 @@
+package dal;
+
+import models.documento.Factura;
+import models.documento.Nota;
+import models.documento.OrdenPago;
+import models.mediopago.TipoPago;
+
+public class RepoFactory {
+    public static Repository<OrdenPago> getRepoOrdenesPago(){
+        return new Repository<>(OrdenPago.class);
+    }
+
+    public static Repository<TipoPago> getRepoTiposDePago(){
+        return new Repository<>(TipoPago.class);
+    }
+
+    public static Repository<Factura> getRepoFacturas(){
+        return new Repository<>(Factura.class);
+    }
+
+    public static Repository<Nota> getRepoNotas(){
+        return new Repository<>(Nota.class);
+    }
+}
