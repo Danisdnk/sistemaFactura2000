190199505a9ce631c1ccf28e4aa6e92f8920e158
Leandro Herenu <lherenu@coalfire.com>
2021-06-24 18:45:23 -0300

    cache repositorios

--- src/dal/RepoFactory.java
+++ src/dal/RepoFactory.java
@@ -7,11 +7,31 @@ import models.mediopago.TipoPago;
 import models.proveedor.Proveedor;
 
 public class RepoFactory {
-    public static Repository<OrdenPago> getRepoOrdenesPago(){  return new Repository<>(OrdenPago.class);  }
+    private static Repository<OrdenPago> ordenPagoRepository;
+    private static Repository<TipoPago> tipoPagoRepository;
+    private static Repository<Factura> facturaRepository;
+    private static Repository<Nota> notaRepository;
+    private static Repository<Proveedor> proveedorRepository;
 
-    public static Repository<TipoPago> getRepoTiposDePago(){  return new Repository<>(TipoPago.class);  }
+    public static Repository<OrdenPago> getRepoOrdenesPago(){
+        if (ordenPagoRepository == null) {
+            ordenPagoRepository = new Repository<>(OrdenPago.class);
+        }
+
+        return  ordenPagoRepository;
+    }
+
+    public static Repository<TipoPago> getRepoTiposDePago(){
+        if (tipoPagoRepository == null) {
+            tipoPagoRepository = new Repository<>(TipoPago.class);
+        }
+
+        return tipoPagoRepository;
+    }
 
     public static Repository<Factura> getRepoFacturas(){  return new Repository<>(Factura.class); }
+
     public static Repository<Nota> getRepoNotas(){  return new Repository<>(Nota.class);  }
+
     public static Repository<Proveedor> getRepoProveedores(){  return new Repository<>(Proveedor.class); }
 }
