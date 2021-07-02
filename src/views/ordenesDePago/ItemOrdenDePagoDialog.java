3b79cef624911fccf5456fffae6ec3d868210267
Leandro Herenu <lherenu@coalfire.com>
2021-07-02 17:19:21 -0300

    carga item op

--- src/views/ordenesDePago/ItemOrdenDePagoDialog.java
+++ src/views/ordenesDePago/ItemOrdenDePagoDialog.java
@@ -1,11 +1,12 @@
 package views.ordenesDePago;
 
 import controllers.ControladorComprobantes;
+import controllers.ControladorOrdenesDePagos;
 import models.documento.Comprobante;
-import models.documento.Factura;
 import models.documento.OrdenPago;
-import models.dtos.ComprobanteDTO;
 import models.dtos.DDLItemDTO;
+import models.documento.ItemOrdenPago;
+import models.mediopago.TipoPago;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
@@ -15,19 +16,21 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ComprobantesAsociadosDialog extends JDialog implements ActionListener  {
+public class ItemOrdenDePagoDialog extends JDialog implements ActionListener  {
     private JPanel main;
-    private JTable tbFacsAsoc;
+    private JTable tbCompsAsoc;
     private JButton btnAsociar;
     private JComboBox ddlComprobantesSinAsoc;
     private JButton btnGuardar;
+    private JComboBox ddlTiposPago;
 
+    private TipoPago tipoDePago;
     private List<Comprobante> comprobantesAsociados;
     private List<Comprobante> comprobantesSinAsociar;
     private Comprobante seleccionado;
     private Integer indiceSeleccionado;
 
-    public ComprobantesAsociadosDialog(Dialog owner, int provID, OrdenPago op) {
+    public ItemOrdenDePagoDialog(Dialog owner, List<Comprobante> comprobantes, ItemOrdenPago itemOrdenPago) {
         super(owner);
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         this.setContentPane(main);
@@ -36,17 +39,15 @@ public class ComprobantesAsociadosDialog extends JDialog implements ActionListen
 
         this.comprobantesAsociados = new ArrayList<>();
 
-        var comprobantes = ControladorComprobantes
-                .getInstancia()
-                .getComprobantesByProveedor(provID);
-
-        if (op != null){
-            this.comprobantesAsociados = op.getComprobantesAsociados();
+        if (itemOrdenPago != null){
+            this.comprobantesAsociados = itemOrdenPago.getComprobantesAsociados();
+            this.ddlTiposPago.setSelectedItem(itemOrdenPago.getTipoDePago().toDDL());
         }
 
         this.setTable();
 
         this.setDDLComprobantesSinAsoc(comprobantes);
+        this.setDDLFormasPago();
 
         this.ddlComprobantesSinAsoc.addActionListener(new ActionListener() {
             @Override
@@ -83,35 +84,21 @@ public class ComprobantesAsociadosDialog extends JDialog implements ActionListen
         this.btnGuardar.addActionListener(this);
     }
 
+    //Guardar
     @Override
     public void actionPerformed(ActionEvent e) {
+        var tipo = (DDLItemDTO)this.ddlTiposPago.getSelectedItem();
+
+        this.tipoDePago = ControladorOrdenesDePagos.getInstancia().getTipoDePagoPorID(tipo.id);
+
         setVisible(false);
         dispose();
     }
 
-    public List<Comprobante> showDialog() {
+    public ItemOrdenPago showDialog() {
         setVisible(true);
-        return this.comprobantesAsociados;
-    }
 
-    private void createTable(){
-        var model = new DefaultTableModel(getHeaderTabla(), 1);
-        this.tbFacsAsoc = new JTable();
-    }
-
-    private void setTable(){
-        var comps = this.comprobantesAsociados;
-
-        DefaultTableModel dm = new DefaultTableModel();
-
-        var dataVector = new Object[comps.size()][2];
-        for (int i = 0; i < comps.size(); i++) {
-            dataVector[i] = crearObjTabla(comps.get(i));
-        }
-
-        dm.setDataVector(dataVector, getHeaderTabla());
-
-        this.tbFacsAsoc.setModel(dm);
+        return this.tipoDePago != null ? new ItemOrdenPago(this.tipoDePago, this.comprobantesAsociados) : null;
     }
 
     private void setDDLComprobantesSinAsoc(List<Comprobante> comprobantes) {
@@ -127,9 +114,34 @@ public class ComprobantesAsociadosDialog extends JDialog implements ActionListen
                         .toArray()));
     }
 
+    private void setDDLFormasPago() {
+        var model = ControladorOrdenesDePagos.getInstancia().getOpcionesDDLFormasDePago();
+        this.ddlTiposPago.setModel(new DefaultComboBoxModel(model.toArray()));
+    }
+
     private void addRow(Comprobante comp) {
-        DefaultTableModel model = (DefaultTableModel) this.tbFacsAsoc.getModel();
+        DefaultTableModel model = (DefaultTableModel) this.tbCompsAsoc.getModel();
         model.addRow(crearObjTabla(comp));
+    }
+
+    private void createTable(){
+        var model = new DefaultTableModel(getHeaderTabla(), 1);
+        this.tbCompsAsoc = new JTable();
+    }
+
+    private void setTable(){
+        var comps = this.comprobantesAsociados;
+
+        DefaultTableModel dm = new DefaultTableModel();
+
+        var dataVector = new Object[comps.size()][2];
+        for (int i = 0; i < comps.size(); i++) {
+            dataVector[i] = crearObjTabla(comps.get(i));
+        }
+
+        dm.setDataVector(dataVector, getHeaderTabla());
+
+        this.tbCompsAsoc.setModel(dm);
     }
 
     private  Object[] crearObjTabla(Comprobante comp){
@@ -137,7 +149,7 @@ public class ComprobantesAsociadosDialog extends JDialog implements ActionListen
     }
 
     private Object[] getHeaderTabla() {
-        return new Object[] { "Fecha", "Tipo - Nro", "Monto" };
+        return new Object[] { "Fecha", "Tipo - Nro", "Total" };
     }
 
     private void createUIComponents() {
