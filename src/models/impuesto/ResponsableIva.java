0f1602d67dfc9bb13eb1d7733c4d172523430c16
danisdnk <danisdnk@gmail.com>
2021-07-04 04:26:54 -0300

    cambio enum por por una clase para poder crearle un repo

--- src/models/impuesto/ResponsableIva.java
+++ src/models/impuesto/ResponsableIva.java
@@ -1,7 +1,39 @@
 package models.impuesto;
-import models.proveedor.Proveedor;
+import dal.Identificable;
+import models.dtos.DDLItemDTO;
+import models.dtos.DDLeable;
 
-public enum ResponsableIva {
-    RES_INSCRIPTO,
-    MONOTRIBUTO
+// TODO agregar a diagrama clases
+
+public class ResponsableIva implements DDLeable, Identificable {
+    private int ID;
+
+    private String type;
+
+    public ResponsableIva(String type) {
+        this.type = type;
+    }
+
+    public DDLItemDTO toDDL(){
+        return new DDLItemDTO(){
+            {
+                id = ID;
+                descripcion = type;
+            }
+        };
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    @Override
+    public int getID() {
+        return ID;
+    }
+
+    @Override
+    public void setID(int ID) {
+        this.ID = ID;
+    }
 }
